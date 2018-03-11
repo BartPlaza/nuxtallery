e1ae63261a04952f4d3b3884ce6cb6bada3d7227
grzebyk44 <grzebyk44@gmail.com>
2018-03-11 12:59:05 +0100

    'working_on_about_page'

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,19 +1,47 @@
 <template>
-	<span>
-	<input type="number" v-model="angle">
 	<div class="page_container">
 		<div class="carousel_wrapper" :style="{'transform': move}">	
-			<div class="carousel_item first"></div>
-			<div class="carousel_item second"></div>
-			<div class="carousel_item third"></div>
-			<div class="carousel_item fourth"></div>
-			<div class="carousel_item fifth"></div>
-			<div class="carousel_item sixsth"></div>
-			<div class="carousel_item seventh"></div>
-			<div class="carousel_item eight"></div>
+			<transition name="fade">
+				<div class="carousel_item first"
+				v-show="item == 1 || item == 2 || item == 8">Use big public gallery and add to it your own images</div>
+			</transition>
+			<transition name="fade">
+				<div class="carousel_item second"
+				v-show="item == 8 || item == 7 || item == 1">Use your private gallery visible only for you</div>
+			</transition>
+			<transition name="fade">
+				<div class="carousel_item third"
+				v-show="item == 3 || item == 4 || item == 2">Access to more feature when you log in</div>
+			</transition>
+			<transition name="fade">
+				<div class="carousel_item fourth"
+				v-show="item == 2 || item == 1 || item == 3">Search and filter images according to your needs</div>
+			</transition>
+			<transition name="fade">
+			<div class="carousel_item fifth"
+			v-show="item == 5 || item == 4 || item == 6"><p>Give likes for public images</p></div>
+			</transition>
+			<transition name="fade">
+				<div class="carousel_item sixsth"
+				v-show="item == 4 || item == 3 || item == 5"><p>Give likes for public images</p></div>
+			</transition>
+			<transition name="fade">	
+				<div class="carousel_item seventh"
+				v-show="item == 7 || item == 6 || item == 8">Give likes for public images</div>
+			</transition>
+			<transition name="fade">
+				<div class="carousel_item eight"
+			v-show="item == 6 || item == 5 || item == 7"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aut dolore quasi quae praesentium maxime pariatur!</p></div>
+			</transition>
 		</div>
+		<div class="carousel_frame">
+			<div class="carousel_nav">
+				<i class="fas fa-sort-up" @click="roll(-1)"></i>
+				<i class="fas fa-sort-down" @click="roll(1)"></i>
+			</div>
+		</div>
+		<back-button link="/" color="black"></back-button>
 	</div>
-	</span>
 </template>
 
 <script>
@@ -21,12 +49,46 @@
 export default {
 	data: function(){
 		return {
-			angle: 22 
+			angle: 0,
+			item: 1
 		}
 	},
 	computed: {
 		move: function(){
 			return 'rotateX('+this.angle+'deg) rotateY(0deg)'
+		},
+		calcShow: function(){
+			return Math.abs(this.angle&360);
+		}
+	},
+	created: function(){
+		window.addEventListener('wheel',(event)=>{
+			this.roll(event.deltaY);
+		});
+	},
+	methods: {
+		roll: function(deltaY){
+			if(deltaY < 0){
+				this.angle += 45;
+				if(this.item < 8){
+					this.item++
+				} else if (this.item = 8){
+					this.item = 1
+				}
+			}else {
+				this.angle -= 45;
+				if(this.item == 1){
+					this.item = 8;
+				} else if (this.item > 0){
+					this.item--;
+				}
+			}
+			this.removeRoll();
+		},
+		removeRoll: function(){
+			window.addEventListener('wheel', (e)=>{
+				e.preventDefault();
+			});
 		}
 	}
 }
@@ -47,19 +109,27 @@ export default {
 	}
 
 	.carousel_wrapper{
-		width: 100px;
+		width: 500px;
 		height: 100px;
-		background-color: red;
 		position: relative;
 		transform-style: preserve-3d;
-		
+		transition: all 1.5s;
+		z-index: 2;	
 	}
 
 	.carousel_item {
-		width: 100px;
+		width: 500px;
 		height: 100px;
-		background-color: blue;
 		position: absolute;
+		color: black;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		transition: opacity 1s;
+		font-size: 22px;
+		text-transform: uppercase;
+		text-align: center;
+
 	}
 
 	.first{
@@ -67,13 +137,12 @@ export default {
 	}
 
 	.second{
-		transform-origin: 0 25px $z-width-minus;
+		transform-origin: 0 50px $z-width-minus;
 		transform: translateZ($z-width) rotateX(45deg)
 	}
 
 	.third{
-		
-		transform: translateZ($z-width-minus)
+		transform: rotateX(-90deg) translateZ($z-width)
 	}
 
 	.fourth{
@@ -82,13 +151,20 @@ export default {
 	}
 
 	.fifth{
-		
-		transform: rotateX(90deg) translateZ($z-width-minus)
+		transform: rotateX(0deg) rotateY(0deg) translateZ($z-width-minus)
+	}
+
+	.fifth > p {
+		transform: rotateX(180deg);
 	}
 
 	.sixsth{
 		transform-origin: 0 50px $z-width;
 		transform: translateZ($z-width-minus) rotateX(45deg);
+	}
+
+	.sixsth > p{
+		transform: rotateX(180deg);
 	}
 
 	.seventh{
@@ -97,7 +173,36 @@ export default {
 
 	.eight{
 		transform-origin: 0 50px $z-width;
-		transform: translateZ($z-width-minus) rotateX(-45deg);
+		transform: translateZ($z-width-minus) rotateX(-45deg) ;
+	}
+
+	.eight > p {
+		transform: RotateX(180deg);
+	}
+
+	.carousel_frame{
+		position: absolute;
+		background-color: #b5ac49;
+		width: 100%;
+		height: 200px;
+		z-index: 1;
+		background-image: linear-gradient(to top, #3ca55c, #b5ac49);
+		box-shadow: 0 10px 6px -6px #777;
+	}
+
+	.carousel_nav {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		justify-content: center;
+		height: 100%;
+		font-size: 44px;
+		margin-right: 50px;
+	}
+
+	.carousel_nav > i:hover {
+	  cursor: pointer;
+	  color: white;
 	}
 
 
